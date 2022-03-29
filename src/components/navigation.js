060bdaad84da0c421118c940fa90afae2efa4a35
Mr Sam Law <sam@chongland.com>
2022-03-29 12:00:20 +1100

    Contentful env variables

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -7,7 +7,7 @@ const Navigation = () => (
   <nav role="navigation" className={styles.container} aria-label="Main">
     <Link to="/" className={styles.logoLink}>
       <span className={styles.logo} />
-      <span className={styles.navigationItem}>Gatsby Starter Contentful</span>
+      <span className={styles.navigationItem}>SAM LAW</span>
     </Link>
     <ul className={styles.navigation}>
       <li className={styles.navigationItem}>
@@ -17,7 +17,7 @@ const Navigation = () => (
       </li>
       <li className={styles.navigationItem}>
         <Link to="/blog/" activeClassName="active">
-          Blog
+          Stories
         </Link>
       </li>
     </ul>
